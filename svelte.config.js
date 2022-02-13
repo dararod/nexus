851968e3f10d360e3b8eb0fc2f936676e6655a7a
Esteban Borai <estebanborai@gmail.com>
2022-02-13 19:12:08 -0300

    chore: pin sveltekit and adapter deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// an array of file extensions that should be treated as Svelte components
+	extensions: ['.svelte'],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: [],
 	kit: {
 		adapter: adapter()
 	}
